784d602bbac9d07010c53ee3cbe9c7fc507b9a90
Karun Japhet <karun@japhet.in>
2017-11-01 19:49:53 -0400

    Create basic Khor processor

--- src/main/kotlin/me/karun/Khor.kt
+++ src/main/kotlin/me/karun/Khor.kt
@@ -1,5 +1,22 @@
 package me.karun
 
-class Khor {
-  fun number() = 1
+open class Khor
+
+class KhorProcessor {
+  fun methods(khorInstance: Khor): Int {
+    return khorInstance.javaClass.methods
+      .filter { m -> m.isAnnotationPresent(Desc::class.java) }
+      .count()
+  }
+
+  fun helpText(khorInstance: Khor): Iterable<HelpTextItem> {
+    return khorInstance.javaClass.methods
+      .filter { it.isAnnotationPresent(Desc::class.java) }
+      .map { HelpTextItem(it.name, it.getAnnotation(Desc::class.java).description) }
+      .toList()
+  }
 }
+
+annotation class Desc(val description: String)
+
+data class HelpTextItem(private val methodName: String, private val description: String)
--- src/test/kotlin/me/karun/KhorTest.kt
+++ src/test/kotlin/me/karun/KhorTest.kt
@@ -1,5 +1,7 @@
 package me.karun
 
+import org.amshove.kluent.`should be`
+import org.amshove.kluent.`should contain all`
 import org.amshove.kluent.shouldBe
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
@@ -8,14 +10,38 @@ import org.jetbrains.spek.api.dsl.on
 
 object KhorTest : Spek({
 
-  given("a khor") {
-    val khor = Khor()
+  given("A CLI implementation using Khor") {
+    val cli: Khor = MyCLI()
 
-    on("performing a check") {
+    on("being processed") {
+      val processor = KhorProcessor()
 
-      it("passes") {
-        khor.number() shouldBe 1
+      it("can count the number of methods") {
+        processor.methods(cli) shouldBe 2
+      }
+
+      it("provide a readable description of the methods") {
+        val expected = listOf(
+          HelpTextItem("hello", "say hello to NAME"),
+          HelpTextItem("goodbye", "say goodbye to NAME")
+        )
+        val actual = processor.helpText(cli)
+
+        actual.count() `should be` expected.count()
+        actual `should contain all` expected
       }
     }
   }
 })
+
+class MyCLI : Khor() {
+  @Desc("say hello to NAME")
+  fun hello(name: String) {
+    println("Hello $name")
+  }
+
+  @Desc("say goodbye to NAME")
+  fun goodbye(name: String) {
+    println("Goodbye $name")
+  }
+}
